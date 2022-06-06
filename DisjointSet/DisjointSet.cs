3a3bf89e10b40f3ff5b6be39c861732ae517bfd9
Markus Staub <markusstaub1@gmail.com>
2022-06-06 18:26:35 +0200

    Klasse DisjointSet: Methode FindIndex, FindRoot, Union implementiert

---
+++ DisjointSet/DisjointSet.cs
@@ -0,0 +1,75 @@
+﻿namespace DisjointSet
+{
+    public class DisjointSetObj
+    {
+        public readonly int[] arrayValue;
+        public readonly int[] arrayIndex;
+
+        public DisjointSetObj()
+        {
+            arrayValue = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            arrayIndex = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        }
+
+
+        public int FindIndex(int value)
+        {
+            for (int i = 0; i < arrayIndex.Length; i++)
+            {
+                if (arrayIndex[i] == value)
+                    return i;
+
+            }
+
+            return 0;
+        }
+
+        public int FindRoot(int value)
+        {
+            int root;
+            int keyIndex = FindIndex(value);
+
+            int keyValue = arrayValue[keyIndex];
+
+            if (keyValue == value)
+            {
+              return root = value;
+            }
+            else
+            {
+             return FindRoot(keyValue);
+            }
+
+        }
+
+        public void Union(int value1, int value2)
+        {
+            var root = 0;
+
+            int keyindex1;
+            int keyindex2;
+
+            keyindex1 = FindIndex(value1);
+            keyindex2 = FindIndex(value2);
+
+            if (value1 < value2)
+            {
+                root = value1;
+
+                arrayValue[keyindex2] = root;
+            }
+            else
+            {
+                root = value2;
+                arrayValue[keyindex1] = root;
+
+            }
+
+
+
+
+        }
+
+    }
+}
--- DisjointSet/Program.cs
+++ DisjointSet/Program.cs
@@ -1,7 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DisjointSet;
+
+
+namespace leetCodePlayground
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            var test = new DisjointSetObj();
+
+
+            test.Union(0, 1);
+            test.Union(0, 2);
+            test.Union(1, 3);
+
+            
+
+            Console.WriteLine(""+test.FindRoot(3));
+
+            Console.ReadKey();
 
 
 
-int[] arrayValue = new int[10];
-int [] arrayIndex = new int[10];
+        }
+    }
+}
