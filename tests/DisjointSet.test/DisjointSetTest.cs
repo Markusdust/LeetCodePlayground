51387053ef910630b42c6107b2d8f3dfa08f241d
Markus Staub <markusstaub1@gmail.com>
2022-06-06 22:40:54 +0200

    DisjointSetObj Methode implementiert CheckVerticesConnected inklusive Test

--- tests/DisjointSet.test/DisjointSetTest.cs
+++ tests/DisjointSet.test/DisjointSetTest.cs
@@ -58,11 +58,41 @@ namespace DisjointSet.test
         public void CheckIfConnected_0And3_ThenReturn_True()
         {
             //Arrange
+            var disjointSet = new DisjointSetObj();
+
+            disjointSet.Union(0, 1);
+            disjointSet.Union(0, 2);
+            disjointSet.Union(1, 3);
+            disjointSet.Union(4, 8);
+            disjointSet.Union(5, 6);
+            disjointSet.Union(5, 7);
 
             //Act
+            var result = disjointSet.CheckVerticesConnected(0, 3);
 
             //Assert
+            result.Should().Be(true);
 
+        }
+
+        [Fact]
+        public void CheckIfConnected_1And5_ThenReturn_False()
+        {
+            //Arrange
+            var disjointSet = new DisjointSetObj();
+
+            disjointSet.Union(0, 1);
+            disjointSet.Union(0, 2);
+            disjointSet.Union(1, 3);
+            disjointSet.Union(4, 8);
+            disjointSet.Union(5, 6);
+            disjointSet.Union(5, 7);
+
+            //Act
+            var result = disjointSet.CheckVerticesConnected(1, 5);
+
+            //Assert
+            result.Should().Be(false);
 
         }
 
